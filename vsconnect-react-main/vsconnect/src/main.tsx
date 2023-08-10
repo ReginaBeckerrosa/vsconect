a89d1dac2059ea00d4c90ee92da8c73d90582ca6
Regina De Aguiar Becker Rosa <beckerrosakkph6@gmail.com>
2023-08-09 21:49:14 -0300

    componets

--- vsconnect-react-main/vsconnect/src/main.tsx
+++ vsconnect-react-main/vsconnect/src/main.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 //componetes
+
 import Home from "./pages/Home/index";
+import ListadeServicos from './pages/ListaDeServicos';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+
 // import ListadeServico from "./pages/ListaDeServicos/index";
 
 //estilizacao chamar o css
@@ -9,8 +15,12 @@ import "./index.css";
 
 
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Header/>
     <Home />
+    {/* <ListadeServicos/> */}
+    <Footer/>
   </React.StrictMode>
 )
